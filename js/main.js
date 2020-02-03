a127d3191e3740fb280e63f6392b3ee6d2fc29cb
Shaun Bolak <47017803+shonb6570@users.noreply.github.com>
2020-02-03 12:10:35 -0500

    added search plugin

---
+++ js/main.js
@@ -0,0 +1,31 @@
+lightbox.option({
+    'wrapAround': true,
+    'imageFadeDuration': 800,
+    'alwaysShowNavOnTouchDevices': true,
+  });
+
+  $( '.flex-container' ).searchable({
+    selector      : '.lb-caption',
+    searchField   : '.search',
+    oddRow        : { 'background-color': '#f5f5f5' },
+    evenRow       : { 'background-color': '#fff' },
+    hide          : function( elem ) {
+        elem.fadeOut(50);
+    },
+    show          : function( elem ) {
+        elem.fadeIn(50);
+    },
+    searchType    : 'fuzzy',
+    onSearchActive : function( elem, term ) {
+        elem.show();
+    },
+    onSearchEmpty: function( elem ) {
+        elem.hide();
+    },
+    onSearchFocus: function() {
+        $( '#feedback' ).show().text( 'Type to search.' );
+    },
+    onSearchBlur: function() {
+        $( '#feedback' ).hide();
+    },
+});
