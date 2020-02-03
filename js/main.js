6c9a8355a9a4b5066a14d46539864ae022aa41f1
Shaun Bolak <47017803+shonb6570@users.noreply.github.com>
2020-02-03 16:27:42 -0500

    search filter plugin removed

--- js/main.js
+++ js/main.js
@@ -3,26 +3,3 @@ lightbox.option({
     'imageFadeDuration': 800,
     'alwaysShowNavOnTouchDevices': true,
   });
-
-  $( '.flex-container' ).searchable({
-    selector      : '.flex-item',
-    searchField   : '.search',
-    oddRow        : { 'background-color': '#f5f5f5' },
-    evenRow       : { 'background-color': '#fff' },
-    hide          : function( elem ) {
-        elem.fadeOut(50);
-    },
-    show          : function( elem ) {
-        elem.fadeIn(50);
-    },
-    searchType    : 'fuzzy',
-    onSearchActive : function( elem, term ) {
-        elem.show();
-    },
-    onSearchFocus: function() {
-        $( '#feedback' ).show().text( 'Type to search.' );
-    },
-    onSearchBlur: function() {
-        $( '#feedback' ).hide();
-    },
-});
