722513e6041c34940d9a2caff19a50a21c2785ae
Shaun Bolak <47017803+shonb6570@users.noreply.github.com>
2020-02-03 18:01:54 -0500

    updated js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@ lightbox.option({
     'imageFadeDuration': 800,
     'alwaysShowNavOnTouchDevices': true,
   });
+
+
+// Functions   //
+
+// Search filter
+
+${'.search'}.on("keyup", function(){
+    var term = $(this).val().toLowerCase();
+    console.log(term);
+    // ${'.flex-item'}.filter(function() {
+    //     $(this).toggle($(this).text().toLowerCase().indexOf(term) > -1)
+    // });
+});
