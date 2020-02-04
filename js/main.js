8aea7514ea82267918bd6e148c1353743b3ff7d7
Shaun Bolak <47017803+shonb6570@users.noreply.github.com>
2020-02-03 19:12:33 -0500

    updated js

--- js/main.js
+++ js/main.js
@@ -9,10 +9,34 @@ lightbox.option({
 
 // Search filter
 
-${'.search'}.on("keyup", function(){
-    var term = $(this).val().toLowerCase();
-    console.log(term);
-    // ${'.flex-item'}.filter(function() {
-    //     $(this).toggle($(this).text().toLowerCase().indexOf(term) > -1)
-    // });
+const one = $('.search').val();
+
+
+$('.search').on('keyup',function() {
+        var term = $(this).val().toLowerCase();
+        $('.flex-item alt').filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(term) > -1)
+        });
 });
+
+
+// const search = document.querySelector('.search');
+// const imgTexts = document.querySelectorAll('.flex-item alt');
+
+// const handleSearch = event => {
+//   const searchTerm = event.target.value.toLowerCase();
+  
+//   imgTexts.forEach(boxText => {
+//     const text = boxText.textContent.toLowerCase();
+//     const photo = boxText.parentElement;
+    
+//     if(text.indexOf(searchTerm) > -1) {
+//       photo.style.display = "block";
+//     } else {
+//       photo.style.display = "none";  
+//     }
+//   });
+
+// };
+
+// search.addEventListener('keyup', handleSearch);
