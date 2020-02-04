de2ff075d79c005467aa08094d955d182cd0c521
Shaun Bolak <47017803+shonb6570@users.noreply.github.com>
2020-02-04 14:37:36 -0500

    added mobile mediaquery for lightbox

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 lightbox.option({
     'wrapAround': true,
     'imageFadeDuration': 800,
+    'maxHeight': 600,
     'alwaysShowNavOnTouchDevices': true,
   });
 
@@ -9,34 +10,9 @@ lightbox.option({
 
 // Search filter
 
-const one = $('.search').val();
-
-
 $('.search').on('keyup',function() {
         var term = $(this).val().toLowerCase();
-        $('.flex-item alt').filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(term) > -1)
+        $('.flex-item').filter(function() {
+            $(this).toggle($(this).attr("data-title").toLowerCase().indexOf(term) > -1)
         });
 });
-
-
-// const search = document.querySelector('.search');
-// const imgTexts = document.querySelectorAll('.flex-item alt');
-
-// const handleSearch = event => {
-//   const searchTerm = event.target.value.toLowerCase();
-  
-//   imgTexts.forEach(boxText => {
-//     const text = boxText.textContent.toLowerCase();
-//     const photo = boxText.parentElement;
-    
-//     if(text.indexOf(searchTerm) > -1) {
-//       photo.style.display = "block";
-//     } else {
-//       photo.style.display = "none";  
-//     }
-//   });
-
-// };
-
-// search.addEventListener('keyup', handleSearch);
